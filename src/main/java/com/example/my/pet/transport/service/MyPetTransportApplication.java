b4d0c46ed0ef6fe4a4f494e1e7c12b6fce2ba532
mypet <191966009@qq.com>
2021-10-20 21:04:23 +0800

    first commit

---
+++ src/main/java/com/example/my/pet/transport/service/MyPetTransportApplication.java
@@ -0,0 +1,13 @@
+package com.example.my.pet.transport.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPetTransportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPetTransportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/my/pet/transport/service/MyPetTransportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my.pet.transport.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPetTransportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
