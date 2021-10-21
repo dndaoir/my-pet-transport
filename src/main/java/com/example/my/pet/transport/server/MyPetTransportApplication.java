a8ede1042b673fa63700c12e074d1fcaa0ed3fe6
mypet <191966009@qq.com>
2021-10-21 20:03:29 +0800

    create package with every floor

--- src/main/java/com/example/my/pet/transport/server/MyPetTransportApplication.java
+++ src/main/java/com/example/my/pet/transport/server/MyPetTransportApplication.java
@@ -1,4 +1,4 @@
-package com.example.my.pet.transport.service;
+package com.example.my.pet.transport.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/my/pet/transport/server/MyPetTransportApplicationTests.java
+++ src/test/java/com/example/my/pet/transport/server/MyPetTransportApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.my.pet.transport.service;
+package com.example.my.pet.transport.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
